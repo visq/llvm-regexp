abac396d9c32b7f397498c6aee48c104dd6db95a
Benedikt Huber <benedikt.huber@gmail.com>
2010-07-29 14:56:04 +0200

    Compute isFinalState only at the end of the string

--- RegExpLLVM.hs
+++ RegExpLLVM.hs
@@ -1,4 +1,15 @@
--- Demo: LLVM Regexp matcher; (c) 2010, Benedikt Huber <benedikt.huber@gmail.com>
+{-# LANGUAGE ScopedTypeVariables #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  RegExpLLVM
+-- Copyright   :  (c) 2010 Benedikt Huber
+-- License     :  BSD-style
+-- Stability   :  Early Demo
+-- Portability :  Test with ghc-6.10
+-- Dependencies : bytestring, llvm
+-- 
+-- Demo: Generating LLVM code to match regular expressions
+-----------------------------------------------------------------------------
 import Prelude hiding (and,or)
 import Control.Monad
 import Control.Monad.State
@@ -23,7 +34,8 @@ data Reg c = Eps
          | Alt (Reg c) (Reg c)
          | Seq (Reg c) (Reg c)
          | Rep (Reg c)
-
+         deriving (Show,Read)
+         
 rep :: Int -> Reg c -> Reg c
 rep 0 _ = Eps
 rep 1 r = r
@@ -66,50 +78,64 @@ finalStates r = case r of
   Eps       -> []
   Rep r     -> finalStates r
 
+-- | Run the given low-level query, which takes an array of characters and returns
+-- a boolean value, on the given bytestring
 runOnString :: (Ptr Word8 -> Word32 -> IO Word32) -> BS.ByteString -> IO Bool
 runOnString f bs = do
   let (fptr,offset,len) = toForeignPtr bs
   r <- withForeignPtr fptr $ \ptr -> f (ptr `plusPtr` offset) (fromIntegral len)
   return (r > 0)
 
+-- | Generate code for matching the given regular expression
 regexMatcher :: Reg Char -> CodeGenModule (Function (Ptr Word8 -> Word32 -> IO Word32))
 regexMatcher re = do
   createNamedFunction ExternalLinkage "matcher" $ \string len -> do
-    let arrSize = fromIntegral (count re) :: Word32
-    arr <- arrayAlloca arrSize :: CodeGenFunction r (Value (Ptr Word32))  -- allocate arr
-    forLoop (w32 0) (w32 arrSize) () $ \ix _ -> do                        -- memset all elements in arr to 0
+    -- basic block declarations
+    top <- getCurrentBasicBlock
+    arrayInitStart <- newNamedBasicBlock "arrayinit_start"
+    loop <- newNamedBasicBlock "loop"
+    body <- newNamedBasicBlock "body"
+    computeFinal <- newNamedBasicBlock "computefinal"
+    exit <- newNamedBasicBlock "exit"
+
+    -- check for empty string
+    let final_empty = valueOf (empty re)
+    is_empty <- icmp IntEQ len (w32 0)                                    -- check if len==0
+    condBr is_empty exit arrayInitStart                                   -- return if empty
+    
+    defineBasicBlock arrayInitStart
+    let arrSize = fromIntegral (count re) :: Word32                       -- size of the state array
+    arr <- arrayAlloca arrSize :: CodeGenFunction r (Value (Ptr Word32))  -- allocate array
+    forLoop (w32 0) (w32 arrSize) () $ \ix _ -> do                        -- memset all elements in array to 0
       ap <- getElementPtr arr (ix, ())
       store (valueOf 0) ap
-
-    top <- getCurrentBasicBlock                                           -- loop initialization stuff
-    loop <- newBasicBlock
-    body <- newBasicBlock
-    exit <- newBasicBlock
+    arrayInitEnd <- getCurrentBasicBlock                                  -- we are now in a different basic block
     br loop
 
     defineBasicBlock loop
-    first <- phi [(valueOf True, top)]                                    -- initially, first is True
-    final <- phi [(valueOf (if empty re then True else False), top)]      -- initially, top is True if re accepts eps
-    strIx <- phi [(w32 0, top)]                                           -- i = 0
-    t     <- icmp IntEQ strIx len                                         -- exit if i==len
-    condBr t exit body
+    first <- phi [(valueOf True, arrayInitEnd)]                           -- initially, first is True
+    strIx <- phi [(w32 0, arrayInitEnd)]                                  -- i = 0
+    t     <- icmp IntEQ strIx len                                         -- check if i==len
+    condBr t computeFinal body                                            -- enter loop if i!=len
 
     defineBasicBlock body                                                 -- Define the loop body
     strp  <- getElementPtr string (strIx, ())                             -- get character
     ch    <- load (strp :: Value (Ptr (Word8)))                           -- ch = str[i]
-    generateRegexpCode re first arr ch                                    -- generate regexp matcher code
-    final' <- genFinalStateCheck (finalStates $ number re) arr (valueOf False) -- check whether we are in final state 
-    
+    generateRegexpCode re first arr ch                                    -- generate regexp matcher code    
     strIx_next <- add strIx (w32 1)                                       -- add 1 to string index
     addPhiInputs strIx [(strIx_next, body)]
     addPhiInputs first [(valueOf False, body)]                            -- first = false from second iteration on
-    addPhiInputs final [(final', body)]                                 
     br loop                                                               -- and loop
-     
-    defineBasicBlock exit
+
+    defineBasicBlock computeFinal
+    let finalIxs = finalStates $ number re
+    final_non_empty <- genFinalStateCheck finalIxs arr (valueOf False)    -- check whether we are in final state 
+    br exit
     
-    final_w32 <- zext final                                               -- LLVM bindings currently do not have Generic Bool
-    ret $ (final_w32 :: Value Word32)
+    defineBasicBlock exit
+    accept <- phi [(final_empty, top), (final_non_empty,computeFinal)]    -- merge accept for empty and non-empty strings
+    (accept32 :: Value Word32) <- zext accept                             -- LLVM bindings (currently) do not have Generic Bool
+    ret accept32
   where
     w32 v   = valueOf v :: Value Word32
 
